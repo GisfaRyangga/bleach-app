4b4fd4a9fbccdbab039667dc1f6b2b328b96dbcd
GisfaRyangga <gisfa.putra2603@mail.ugm.ac.id>
2022-11-28 15:00:25 +0700

    fix bootstrap tambah buku

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -4,7 +4,7 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-<div class="container">
+<div class="container mt-4">
     <h4>Tambah Buku</h4>
     @if (count($errors) > 0)
         <ul class="alert alert-danger">
